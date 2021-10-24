cb7232d74af69885379db9e2198c81a0f24e208e
manchann <chl8273@naver.com>
2021-10-25 05:38:43 +0900

    change file to input func

--- lambda_function.py
+++ lambda_function.py
@@ -54,10 +54,11 @@ def filenames_to_input(file_list):
     imgs = []
     for file in file_list:
         img = read_image_from_s3(file)
-        img = img.resize((224, 224), Image.ANTIALIAS)
+        img = img.resize((240, 240), Image.ANTIALIAS)
         img = np.array(img)
 
         # batchsize, 224, 224, 3
+        print(img)
         img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
         img = preprocess_input(img)
         imgs.append(img)
